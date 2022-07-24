b8dcaca90a541999a373c6c85d25c22e35b00453
simonsuetterlin <simon.suetterlin@gmx.de>
2022-07-24 14:36:00 +0200

    load and save optimizer, bugfix with markov chain

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # from rk4step import rk4step
 import numpy as np
+import pickle
+
+from sqlalchemy import false
 from src.grid_optimizer import GridOptimizer
 from src.simulation import Simulator
 from src.model import GridModel
 from src.help_functions import *
 from src.markov_chain import init_chain
+
+LOAD_OPTIMIZER = True
+save_name = "test"
+load_name = "test"
 
 # set constants: prices, state-space, decision-space
 # and max expected change rate of consumption
@@ -12,15 +19,26 @@ P_e = 30
 P_i = 10
 P_b = 5
 U = np.array([- np.inf,-2, -1, 0, 1, 2, np.inf]) # -inf stands for instant shutdown inf for instant start to 80%
-O = np.array([0, 8, 9, 10])#np.arange(0, 12)
-V = np.arange(21)
-B = np.arange(25)
+O = np.array([0, 8, 9, 10, 11, 12, 13, 14])#np.arange(0, 12)
+V = np.arange(5)#21)
+B = np.arange(10)
 V_max_change = 4
 B_max_charge = max(B)
 
 # instant change of O
 def L_i_instant(x0, x1):
-    return produce_O_instant(x0, x1) * P_i
+    split_O_into_two = [O[i:i+int(len(O)/2)+1] for i in range(0, len(O), int(len(O)/2)+1)]
+    prod_O=produce_O_instant(x0, x1)
+    if x0[0] == 0:
+        return prod_O * P_i * 1.5
+    elif x0[0] in split_O_into_two[0]:
+        return prod_O * P_i * 1.25
+    elif x0[0] in split_O_into_two[1]:
+        return prod_O * P_i
+    else:
+        raise ValueError("x0[0] not in O split array")
+
+    # return produce_O_instant(x0, x1) * P_i
 
 def L_b_instant(x0, x1):
     return battery_usage_instant(x0, x1) * P_b
@@ -52,19 +70,32 @@ def L_b(x0, x1):
 def L_e(x0, x1):
     return (deficit_O(x0, x1) - battery_usage(x0, x1, B_max_charge)) * P_e
 
+def save_optimizer(optimizer, name):
+    with open(f'./model_weights/{name}.pkl', 'wb') as outp:
+        pickle.dump(optimizer, outp, pickle.HIGHEST_PROTOCOL)
+
+def load_optimizer(name):
+    with open(f'./model_weights/{name}.pkl', 'rb') as inp:
+        grid_opt = pickle.load(inp)
+    return grid_opt
+
 
 if __name__ == '__main__':
-    chain = init_chain(np.max(V))
-    
-    #model = GridModel(L_list=[L_i, L_e, L_b], P_i=P_i, P_e=P_e, P_b=P_b,
-    model = GridModel(L_list=[L_i_instant, L_e_instant, L_b_instant], P_i=P_i, P_e=P_e, P_b=P_b,
-                       U=U, O=O, V=V, B=B, V_max_change=V_max_change,
-                       chain=chain)
-    grid_opt = GridOptimizer(model)
-    grid_opt.calculate_cost_to_go_matrix_sequence(depth=5)
+    if not LOAD_OPTIMIZER:
+        chain = init_chain(np.max(V))
+        
+        model = GridModel(L_list=[L_i_instant, L_e_instant, L_b_instant], P_i=P_i, P_e=P_e, P_b=P_b,
+                        U=U, O=O, V=V, B=B, V_max_change=V_max_change,
+                        chain=chain)
+        grid_opt = GridOptimizer(model)
+        grid_opt.calculate_cost_to_go_matrix_sequence(depth=5)
+
+        save_optimizer(grid_opt, save_name)
+    else:
+        grid_opt = load_optimizer(load_name)
 
     # simulate model
-    s = Simulator(model, grid_opt.opt_dec_m)
+    s = Simulator(grid_opt.model, grid_opt.opt_dec_m)
     s.simulate(T=100)
     s.plot_path()
 
--- src/markov_chain.py
+++ src/markov_chain.py
@@ -19,12 +19,14 @@ kwargs = {
 dataframes = (pd.read_csv(name, **kwargs) for name in names)
 
 
-def init_chain(max_output, sample_size=1000):
+def init_chain(max_output, sample_size=50000):
     data = pd.concat(dataframes, ignore_index=True)
     data.dropna(inplace=True)
     data_numeric= pd.to_numeric(data['energy(kWh/hh)'][:sample_size])
-    data_numeric = data_numeric * (max_output+1) / np.max(data_numeric)
+    data_numeric = data_numeric * max_output / np.max(data_numeric)
     np.rint(data_numeric, out=data_numeric)
-
+    data_numeric = data_numeric.astype('int')
+    # if a value is not in the data:
+    data_numeric = np.append(data_numeric, range(max_output + 1))
     chain = mc.MarkovChain().from_data(data_numeric)
     return chain
