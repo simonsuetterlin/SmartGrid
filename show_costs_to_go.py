ad8d71e60cee1dc5ea4e0fbbd5b0be2b65964d30
unknown <simon.ebert.kn@gmx.de>
2022-08-01 15:30:42 +0200

    Modified cost to go display function

--- show_costs_to_go.py
+++ show_costs_to_go.py
@@ -1,7 +1,6 @@
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-from ipywidgets import widgets, interactive
 from main import *
 
 
@@ -14,21 +13,14 @@ grid_opt = load_optimizer(load_name)
 
 M = grid_opt.cost_to_go_m
 
-
-
-#fig, axs = plt.subplots(4, 5, dpi=100, figsize=(20, 12))
-#for i in range(M.shape[1]):
-#    axs[i].step(M[:,i,:])
-
-#show the costs to go for the different states. The consumption states are distributed over the different figures
+#show the costs to go for the different states.
 #
 
 while True:
     print('Enter a value for the consumption state:')
     c = int(input())
     if c not in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]:
-        print('Wrong input!')
-        break
+        raise ValueError('Wrong input!')
     else:
         fig = plt.figure()
         axes = fig.add_subplot(111)
@@ -43,22 +35,6 @@ while True:
         plt.title('The costs to go for different Battery and Power\nplant states with consumption state '+str(c))
         plt.show()
 
-
-for i in [0,1]:#grid_opt.model.V:
-    fig = plt.figure()
-    axes = fig.add_subplot(111)
-    caxes = axes.matshow(M[:,0,:])
-    fig.colorbar(caxes)
-    axes.set_xticks(grid_opt.model.B)
-    axes.set_xticklabels(grid_opt.model.B)
-    axes.set_yticks(np.arange(len(grid_opt.model.O)))
-    axes.set_yticklabels(grid_opt.model.O)
-    axes.set_xlabel('Battery state')
-    axes.set_ylabel('Output state')
-    plt.title('The costs to go for different Battery and Power\nplant states with consumption state '+str(i))
-
-
-plt.show()
 
 
 
